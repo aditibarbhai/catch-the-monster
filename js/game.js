ff2a606d1e92079fcbfbd5b32808d17bfe3d0b94
aditibarbhai <aditi.barbhai@mail.utoronto.ca>
2014-01-24 23:46:30 -0500

    Changed the code and added fixes so the player and monster don’t go off-screen

--- js/game.js
+++ js/game.js
@@ -1,91 +1,141 @@
-// Create the canvas
+// GLOBAL VARS
+var min_X = 0;
+var max_X = 483;
+var min_Y = 0;
+var max_Y = 440;
+
+// create the canvas
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = 512;
 canvas.height = 480;
-document.body.appendChild(canvas);
 
-// Background image
+$(document).ready(function(){
+	document.body.appendChild(canvas);
+});
+
+/* IMAGES */
+// background image 
 var bgReady = false;
 var bgImage = new Image();
-bgImage.onload = function () {
+bgImage.onload = function(){
 	bgReady = true;
 };
 bgImage.src = "images/background.png";
 
-// Hero image
+// hero image
 var heroReady = false;
 var heroImage = new Image();
-heroImage.onload = function () {
+heroImage.onload = function() {
 	heroReady = true;
-};
+}
 heroImage.src = "images/hero.png";
 
-// Monster image
+// monster image
 var monsterReady = false;
 var monsterImage = new Image();
-monsterImage.onload = function () {
+monsterImage.onload = function() {
 	monsterReady = true;
-};
+}
 monsterImage.src = "images/monster.png";
 
-// Game objects
+/* GAME OBJECTS */
 var hero = {
-	speed: 256 // movement in pixels per second
+	speed : 256, // movement in pixels per second
+	x : 0,
+	y : 0
 };
-var monster = {};
-var monstersCaught = 0;
 
-// Handle keyboard controls
+var monster = {	// monsters don't move
+	x : 0,
+	y : 0
+};
+
+var monstersCaught = 0;	// the # of monsters the player has caught
+
+/* HANDLE PLAYER INPUT */
+// keyboard controls
+// store users input for later instead of acting on it immediately
+// if a key code is in the object "keysDown", the user is currently pressing that key
 var keysDown = {};
 
-addEventListener("keydown", function (e) {
+addEventListener("keydown", function(e){
 	keysDown[e.keyCode] = true;
 }, false);
 
-addEventListener("keyup", function (e) {
+addEventListener("keyup", function(e){
 	delete keysDown[e.keyCode];
 }, false);
 
-// Reset the game when the player catches a monster
-var reset = function () {
-	hero.x = canvas.width / 2;
-	hero.y = canvas.height / 2;
+/* NEW GAME */
 
-	// Throw the monster somewhere on the screen randomly
-	monster.x = 32 + (Math.random() * (canvas.width - 64));
-	monster.y = 32 + (Math.random() * (canvas.height - 64));
-};
+// random number generator from MDN:
+/**
+ * Returns a random number between min and max
+ */
+function getRandomArbitary (min, max) {
+    return Math.random() * (max - min) + min;
+}
 
-// Update game objects
-var update = function (modifier) {
-	if (38 in keysDown) { // Player holding up
+
+// Reset the game when the player catches the monster
+// Or use this function to start a new game/level
+var reset = function(){
+	// place hero in middle of screen
+	hero.x = canvas.width/2;
+	hero.y = canvas.height/2;
+
+	// place monster randomly on screen
+	monster.x = getRandomArbitary(0, 440);// 32 + (Math.random() * (canvas.width - 64));
+	console.log(monster.x);
+	monster.y = getRandomArbitary(0, 450); //32 + (Math.random() * (canvas.height - 64));
+	console.log(monster.y);
+}
+
+/* Update Objects */
+// this function is called repeatedly at a set interval
+var update = function(modifier){
+	// arrow key up is pressed
+	if (38 in keysDown) {
 		hero.y -= hero.speed * modifier;
+		// make sure hero doesn't go off screen
+		if (hero.y < min_Y) hero.y = min_Y;
 	}
-	if (40 in keysDown) { // Player holding down
+
+	// arrow key down is pressed
+	if (40 in keysDown) {
 		hero.y += hero.speed * modifier;
+		// make sure hero doesn't go off screen
+		if (hero.y > max_Y) hero.y = max_Y;
 	}
-	if (37 in keysDown) { // Player holding left
+
+	// arrow key left is pressed
+	if (37 in keysDown) {
 		hero.x -= hero.speed * modifier;
+		// make sure hero doesn't go off screen
+		if (hero.x < min_X) hero.x = min_X;
 	}
-	if (39 in keysDown) { // Player holding right
+
+	if (39 in keysDown) { // arrow key right is pressed
 		hero.x += hero.speed * modifier;
+		// make sure hero doesn't go off screen
+		if (hero.x > max_X) hero.x = max_X;
 	}
 
-	// Are they touching?
-	if (
-		hero.x <= (monster.x + 32)
-		&& monster.x <= (hero.x + 32)
-		&& hero.y <= (monster.y + 32)
-		&& monster.y <= (hero.y + 32)
-	) {
-		++monstersCaught;
-		reset();
+	// Check if hero and monster are overlapping on screen
+	if (hero.x <= (monster.x + 32) &&
+		monster.x <= (hero.x + 32) &&
+		hero.y <= (monster.y + 32) &&
+		monster.y <= (hero.y + 32) ) {
+		
+		++monstersCaught;	// increment monsters caught
+		reset();	// reset to show new monster
 	}
-};
+}
 
-// Draw everything
-var render = function () {
+/* RENDER OBJECTS */
+// draw everything
+var render = function() {
 	if (bgReady) {
 		ctx.drawImage(bgImage, 0, 0);
 	}
@@ -98,26 +148,26 @@ var render = function () {
 		ctx.drawImage(monsterImage, monster.x, monster.y);
 	}
 
-	// Score
+	// SCORE
 	ctx.fillStyle = "rgb(250, 250, 250)";
 	ctx.font = "24px Helvetica";
 	ctx.textAlign = "left";
 	ctx.textBaseline = "top";
 	ctx.fillText("Goblins caught: " + monstersCaught, 32, 32);
-};
+}
 
-// The main game loop
-var main = function () {
+/* MAIN GAME LOOP */
+var main = function() {
 	var now = Date.now();
 	var delta = now - then;
 
-	update(delta / 1000);
+	update(delta/1000);
 	render();
 
 	then = now;
-};
+}
 
-// Let's play this game!
+/* START THE GAME */
 reset();
 var then = Date.now();
-setInterval(main, 1); // Execute as fast as possible
+setInterval(main, 1); // execute as fast as possible
